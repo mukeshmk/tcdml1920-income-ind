204dd201c14eed8f79a8cef2913269721b6065da
Mukesh A <amukesh.mk@gmail.com>
2019-10-06 14:14:27 +0100

    modularizing the code

--- linear-regression.py
+++ linear-regression.py
@@ -5,10 +5,12 @@ from sklearn import preprocessing as pp
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 
+
 # Check-out TF-IDF, word2vec, countvectorizer, OneHotEncoder, labelBinarizer, LabelEncoder, OrdinalEncoder
 
 # reading data and handling unknowns
-df = pd.read_csv('tcd ml 2019-20 income prediction training (with labels).csv', na_values={
+def openAndHandleUnknowns(fileName):
+    return pd.read_csv(fileName, na_values={
         'Year of Record': ['#N/A'],
         'Gender': [0, '#N/A', 'unknown'],
         'Age': ['#N/A'],
@@ -20,21 +22,37 @@ df = pd.read_csv('tcd ml 2019-20 income prediction training (with labels).csv', 
         'Hair Color': [0, '#N/A', 'Unknown'],
         'Body Height [cm]': [],
         'Income in EUR': []
-})
+    })
+
 
 # handling NaN
 # try different methods of interpolate to reduce RMSE
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.interpolate.html#pandas.DataFrame.interpolate
-df['Year of Record'] = np.floor(df['Year of Record'].interpolate(method='slinear'))
-df['Gender'].fillna('other', inplace=True)
-df['Age'] = np.floor(df['Age'].interpolate(method='slinear'))
-# different methods of fillna?
-df['Profession'].fillna(method='ffill', inplace=True)
-df['University Degree'].fillna(method='ffill', inplace=True)
-df['Hair Color'].fillna(method='ffill', inplace=True)
+def dfFillNaN(df):
+    df['Year of Record'] = np.floor(df['Year of Record'].interpolate(method='slinear'))
+    df['Gender'].fillna('other', inplace=True)
+    df['Age'] = np.floor(df['Age'].interpolate(method='slinear'))
+    # different methods of fillna?
+    df['Profession'].fillna(method='ffill', inplace=True)
+    df['University Degree'].fillna(method='ffill', inplace=True)
+    df['Hair Color'].fillna(method='ffill', inplace=True)
+    return df
+
+
+# One Hot Encoding
+def oheFeature(feature, encoder, data, df):
+    ohedf = pd.DataFrame(data, columns=[feature + ': ' + str(i.strip('x0123_')) for i in encoder.get_feature_names()])
+    df = pd.concat([df, ohedf], axis=1)
+    del df[feature]
+    return df
+
+
+df = openAndHandleUnknowns('tcd ml 2019-20 income prediction training (with labels).csv')
+
+df = dfFillNaN(df)
 df['Income in EUR'] = df['Income in EUR'].abs()
 
-Y = df['Income in EUR']
+y = df['Income in EUR']
 # features being considered for linear regression
 df = df[['Year of Record', 'Gender', 'Age', 'University Degree', 'Wears Glasses', 'Hair Color', 'Body Height [cm]']]
 # features not considered for linear regression "FOR NOW"
@@ -48,32 +66,26 @@ df = df[['Year of Record', 'Gender', 'Age', 'University Degree', 'Wears Glasses'
 yor_scaler = pp.StandardScaler()
 df['Year of Record'] = yor_scaler.fit_transform(df['Year of Record'].values.reshape(-1, 1))
 
-# One Hot Encoding
+
 ohe_gender = pp.OneHotEncoder(categories='auto', sparse=False)
 ohe_gender_data = ohe_gender.fit_transform(df['Gender'].values.reshape(len(df['Gender']), 1))
-dfOneHotGender = pd.DataFrame(ohe_gender_data, columns=['Gender: '+str(i.strip('x0123_')) for i in ohe_gender.get_feature_names()])
-df = pd.concat([df, dfOneHotGender], axis=1)
-del df['Gender']
+df = oheFeature('Gender', ohe_gender, ohe_gender_data, df)
 
 ohe_degree = pp.OneHotEncoder(categories='auto', sparse=False)
 ohe_degree_data = ohe_degree.fit_transform(df['University Degree'].values.reshape(len(df['University Degree']), 1))
-dfOneHot = pd.DataFrame(ohe_degree_data, columns=['University Degree: '+str(i.strip('x0123_')) for i in ohe_degree.get_feature_names()])
-df = pd.concat([df, dfOneHot], axis=1)
-del df['University Degree']
+df = oheFeature('University Degree', ohe_degree, ohe_degree_data, df)
 
 ohe_hair = pp.OneHotEncoder(categories='auto', sparse=False)
 ohe_hair_data = ohe_hair.fit_transform(df['Hair Color'].values.reshape(len(df['Hair Color']), 1))
-dfOneHotHair = pd.DataFrame(ohe_hair_data, columns=['Hair Color: '+str(i.strip('x0123_')) for i in ohe_hair.get_feature_names()])
-df = pd.concat([df, dfOneHotHair], axis=1)
-del df['Hair Color']
+df = oheFeature('Hair Color', ohe_hair, ohe_hair_data, df)
 
 # can be modified to used k-fold cross validation
-X_train, X_test, Y_train, Y_test = train_test_split(df, Y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random_state=0)
 
 model = linear_model.LinearRegression()
-model.fit(X_train, Y_train)
+model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
 print('Coefficients: \n', model.coef_)
-print("RMSE: %.2f" % np.sqrt(mean_squared_error(Y_test, y_pred)))
-print('Variance score: %.2f' % r2_score(Y_test, y_pred))
+print("RMSE: %.2f" % np.sqrt(mean_squared_error(y_test, y_pred)))
+print('Variance score: %.2f' % r2_score(y_test, y_pred))
